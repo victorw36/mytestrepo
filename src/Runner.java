9289364d352958c4b4dd1d2cff4c509255bb17de
BT_1N3_08 <sample@bths.edu>
2023-12-15 10:48:30 -0500

    bob

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,7 @@ public class Runner {
     public static void main(String[] args ) {
         System.out.print("hello");
         System.out.println("p1 p2 p3 p4 p5 p6");
-        System.out.println("Andre");
+        System.out.println("deeznut");
         System.out.println("ligmab");
     }
 }
